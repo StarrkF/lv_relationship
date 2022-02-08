849a620205c254afc6e5b7bfc2264f488f758258
Muhammed Furkan Aymergen <faymergen98@gmail.com>
2022-02-08 11:08:04 +0300

    get user with relations used dd

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+    protected $table='user';
+
+    public function cat()
+    {
+        return $this->hasOne(Category::class,'id','catid');
+    }
+
+    public function dept()
+    {
+        return $this->hasOne(Department::class,'id','deptid');
+    }
 }
